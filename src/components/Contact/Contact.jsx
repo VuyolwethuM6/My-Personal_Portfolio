c9c780cff189ede748c3cd72964516a6dd88b997
VuyolwethuM6 <vmabhuleka@gmail.com>
2024-02-22 15:04:55 +0200

    Made changes on the form to submit email using emailJs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,10 +3,13 @@ import "./Contact.css";
 import images from '../../assets/images/connect.png';
 import { Formik } from 'formik';
 import ReCAPTCHA from 'react-google-recaptcha';
+import emailjs from 'emailjs-com'; // Import EmailJS library
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const Contact = () => {
-
   // Declare state variables
   const [captchaVerified, setCaptchaVerified] = useState(false);
   const captchaRef = useRef(null);
@@ -49,10 +52,36 @@ const Contact = () => {
                 return errors;
               }}
               onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                  setSubmitting(false);
-                }, 400);
+                emailjs.send('service_4337h57', 'template_d29unxr', values, '2W49r_dF_14TbuO4E')
+                  .then((response) => {
+                    toast.success("Message sent successfully!", {
+                      position: "top-right",
+                      autoClose: 5000,
+                      hideProgressBar: false,
+                      closeOnClick: true,
+                      pauseOnHover: true,
+                      draggable: true,
+                      progress: undefined,
+                      theme: "dark",
+                      });
+                    console.log('Success:', response.status, response.text);
+                  })
+                  .catch((error) => {
+                    toast.error("Failed to send message. Please try again later.", {
+                      position: "top-right",
+                      autoClose: 5000,
+                      hideProgressBar: false,
+                      closeOnClick: true,
+                      pauseOnHover: true,
+                      draggable: true,
+                      progress: undefined,
+                      theme: "dark",
+                      });
+                    console.error('Error:', error);
+                  })
+                  .finally(() => {
+                    setSubmitting(false);
+                  });
               }}
             >
               {({
@@ -63,7 +92,7 @@ const Contact = () => {
                 handleBlur,
                 handleSubmit,
                 isSubmitting,
-                /* and other goodies */
+                
               }) => (
                 <form onSubmit={handleSubmit}>
                   <div className='form-elem'>
@@ -84,11 +113,11 @@ const Contact = () => {
                     <span className='form-control-text'>{errors.message && touched.message && errors.message}</span>
                   </div>
                   <ReCAPTCHA className="captcha"
-                          ref={captchaRef}
-                          sitekey="6LfxvnspAAAAAPgoBb45aXcW10dOU_dAmvLdfq2C"
-                          onChange={(token) => {
-                            setCaptchaVerified(!!token);
-                          }}
+                            ref={captchaRef}
+                            sitekey="6LfxvnspAAAAAPgoBb45aXcW10dOU_dAmvLdfq2C"
+                            onChange={(token) => {
+                              setCaptchaVerified(!!token);
+                            }}
                    />
                   <div className='flex flex-start align-center'>
                     <button type="submit" disabled={isSubmitting} className="submit-btn">Send Message</button>
@@ -96,6 +125,7 @@ const Contact = () => {
                 </form>
               )}
             </Formik>
+            <ToastContainer />
           </div>
         </div>
       </div>
@@ -104,101 +134,3 @@ const Contact = () => {
 }
 
 export default Contact;
-
-
-// import React from 'react';
-// import "./Contact.css";
-// import images from '../../constants/images';
-// import { Formik } from 'formik';
-// import Info from './Info';
-// import Map from './Map';
-
-// const Contact = () => {
-//   return (
-//     <section className='contact section-p-top bg-black' id = "contact">
-//         <div className='container'>
-//             <div className='contact-content grid text-center'>
-//                 <div className='contact-left'>
-//                     <div className='section-t'>
-//                         <h3>Let's Talk?</h3> 
-//                         <p className='text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, assumenda quia repellendus architecto culpa nisi?</p>
-//                     </div>
-
-//                     <Formik
-//                         initialValues={{ name: "", email: '', address: '' }}
-//                         validate={values => {
-//                             const errors = {};
-
-//                             if(!values.name){
-//                                 errors.name = "Name is required";
-//                             } else if(!/^[A-Za-z\s]*$/.test(values.name)){
-//                                 errors.name = "Invalid name format";
-//                             }
-
-//                             if (!values.email) {
-//                                 errors.email = 'Email is required';
-//                             } else if (
-//                             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-//                             ) {
-//                                 errors.email = 'Invalid email address';
-//                             }
-
-//                             if(!values.address){
-//                                 errors.address = "Address is required";
-//                             }
-//                             return errors;
-//                         }}
-//                         onSubmit={(values, { setSubmitting }) => {
-//                             setTimeout(() => {
-//                             alert(JSON.stringify(values, null, 2));
-//                             setSubmitting(false);
-//                             }, 400);
-//                         }}
-//                         >
-//                         {({
-//                             values,
-//                             errors,
-//                             touched,
-//                             handleChange,
-//                             handleBlur,
-//                             handleSubmit,
-//                             isSubmitting,
-//                             /* and other goodies */
-//                         }) => (
-//                             <form onSubmit={handleSubmit}>
-//                                 <div className='form-elem'>
-//                                     <input type = "text" name = "name" onChange = {handleChange} onBlur = {handleBlur} value = {values.name} className = "form-control" />
-//                                     <span className='form-control-text'>{errors.name && touched.name && errors.name}</span>
-//                                 </div>
-
-//                                 <div className='form-elem'>
-//                                     <input type = "email" name = "email" onChange = {handleChange} onBlur = {handleBlur} value = {values.email} className = "form-control" />
-//                                     <span className='form-control-text'>{errors.email && touched.email && errors.email}</span>
-//                                 </div>
-
-//                                 <div className='form-elem'>
-//                                     <input type = "text" name = "address" onChange = {handleChange} onBlur = {handleBlur} value = {values.address} className = "form-control" />
-//                                     <span className='form-control-text'>{errors.address && touched.address && errors.address}</span>
-//                                 </div>
-
-//                                 <div className='flex flex-start'>
-//                                     <button type = "submit" disabled = {isSubmitting} className = "submit-btn">contact us</button>
-//                                 </div>
-//                             </form>
-//                         )}
-//                         </Formik>
-//                 </div>
-
-//                 <div className='contact-right'>
-//                     <img src = {images.form_main_img} alt = "" />
-//                 </div>
-//             </div>
-//         </div>
-
-//         <Map />
-//         <Info />
-//     </section>
-//   )
-// }
-
-// export default Contact
