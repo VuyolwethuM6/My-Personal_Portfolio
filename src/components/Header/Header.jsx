7046f6de65012e8200f021e5b3964201fd078d59
VuyolwethuM6 <vmabhuleka@gmail.com>
2024-02-23 13:29:51 +0200

    Added the CV on the download button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,10 @@ import "./Header.css";
 import { FaDownload } from "react-icons/fa";
 
 const Header = () => {
+
+  function downloadCV(){
+    window.open("https://drive.google.com/file/d/1sFpXlp7NYqVgSTI5QY41cA_zaW8qb5O0/view?usp=sharing", "_blank");
+  }
   return (
     <Motion defaultStyle={{ opacity: 0, translateY: -50, slideInLeft: -50, slideInRight: 50 }} style={{ opacity: spring(1), translateY: spring(0), slideInLeft: spring(0), slideInRight: spring(0) }}>
       {interpolatingStyle => (
@@ -18,7 +22,7 @@ const Header = () => {
             <div className='header-content text-center flex flex-column'>
               <h1 className='text-uppercase header-title' style={{ transform: `translateY(${interpolatingStyle.translateY}%)` }}>the Ideal Junior Developer</h1>
               <p className='text-lead' style={{ transform: `translateX(${interpolatingStyle.slideInLeft}%)` }}>A proactive learner with strong problem-solving abilities, excellent communication skills, and a passion for contributing positively to team projects while continuously striving for growth and improvement.</p>
-              <a href="/" className='btn header-btn btn-blue' style={{ transform: `translateX(${interpolatingStyle.slideInRight}%)` }}>
+              <a href="/" onClick={downloadCV} className='btn header-btn btn-blue' style={{ transform: `translateX(${interpolatingStyle.slideInRight}%)` }}>
                 <FaDownload /> <span>Download CV</span>
               </a>
             </div>
