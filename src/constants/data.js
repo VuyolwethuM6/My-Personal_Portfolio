0b86f3682a5d788b320d69b290dba253eb0c4e69
Vuyolwethu Mabhuleka <142888677+VuyolwethuM6@users.noreply.github.com>
2024-04-29 23:32:17 +0200

    Update contact section on data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -95,26 +95,6 @@ const testimonials = [
     }
 ]
 
-const contact = [
-    {
-        id: 25,
-        icon: <FaPhoneAlt style = {{ fill: gradient }} />,
-        info: "+425 235 712",
-        text: "Lorem ipsum dolor sit mattis amet consectetur adipiscing"
-    },
-    {
-        id: 26,
-        icon: <FaEnvelopeOpen style = {{ fill: gradient }} />,
-        info: "solnhub@info.com",
-        text: "Lorem ipsum dolor sit mattis amet consectetur adipiscing"
-    },
-    {
-        id: 27,
-        icon: <FaMapMarkerAlt style = {{ fill: gradient }} />,
-        info: "United Kingdom, New Street",
-        text: "Lorem ipsum dolor sit mattis amet consectetur adipiscing"
-    }
-]
 const projects = [
     {
       name: "Car Rent",
