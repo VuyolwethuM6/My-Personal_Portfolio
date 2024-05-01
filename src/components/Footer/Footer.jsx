14894e27a8f96c67b8b649cd676dc58741935c29
Vuyolwethu Mabhuleka <142888677+VuyolwethuM6@users.noreply.github.com>
2024-05-01 15:07:12 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,14 +11,6 @@ const Footer = () => {
         <div className='grid footer-content text-center'>
           <p className='text'>All Rights Reserved.</p>
           <span className='text'>&copy; {date.getFullYear().toString()} Vuyolwethu Mabhuleka</span>
-          {/* <ul className='navbar-social flex '>
-            <li className='text-white'>
-              <a href="" className='flex flex-center'><FaLinkedin /></a>
-            </li>
-            <li className='text-white'>
-              <a href="https://github.com/VuyolwethuM6" className='flex flex-center'><FaGithub /></a>
-            </li>
-          </ul> */}
         </div>
       </div>
     </footer>
